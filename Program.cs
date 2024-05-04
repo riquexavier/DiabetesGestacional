f69831cc33c1919d018ff4398ffbdbd532fc5e21
ETEC Adolpho Berezin <110746209+riquexavier@users.noreply.github.com>
2024-05-03 22:16:57 -0300

    Pedrão das glicose das gravidez

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+int glicose;
+
+Console.Write("Digite o valor de glicemia (em mg/dL): ");
+glicose = Convert.ToInt32(Console.ReadLine());
+
+if (glicose < 92)
+{
+Console.Write("reavaliar entre 24 e 28 semanas com TTOG 75g de glicose ");
+}
+
+else if (glicose >= 92 &&  glicose < 126)
+{
+    Console.Write("Compatível com Diabetes Gestacional");
+}
+else
+{
+    Console.Write("Diabetes Mellitus na Gravidez");
+}
